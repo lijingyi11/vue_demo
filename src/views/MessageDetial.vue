ab2e631e973a9992558d359e03a99c63fb582cd0
lijingyi11 <1002734313@qq.com>
2019-07-29 16:50:55 +0800

    vue-路由-数据传输

---
+++ src/views/MessageDetial.vue
@@ -0,0 +1,41 @@
+<template>
+   <div>
+    <p>{{$route.params.id}}</p>
+    <p>id:::{{msg.id}}</p>
+    <p>title:::{{msg.title}}</p>
+    <p>content:::{{msg.content}}</p>
+   </div>
+</template>
+
+<script>
+    const msgArr =[
+      {id: 1 ,title:'msg01',content:'11...'},
+      {id:2,title:'msg02',content:'22...'},
+      {id:3,title:'msg03',content:'33...'} ,
+      {id:4,title:'msg04',content:'44...'}
+
+    ];
+    export default {
+      data(){
+        return {
+          msg:{}
+        }
+      },
+      mounted(){
+        setTimeout(() => {
+          const id = this.$route.params.id;
+          this.msg = msgArr.find(msg => msg.id===id*1 );
+        },1000)
+      },
+      watch:{
+        $route:function (value) {
+          const id = value.params.id;
+          this.msg = msgArr.find(msg => msg.id===id*1 );
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
