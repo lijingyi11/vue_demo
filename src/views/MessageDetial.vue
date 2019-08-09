bb0cb14abd6b74543da6940a88a79c4b870d0a33
lijingyi11 <1002734313@qq.com>
2019-08-09 10:51:19 +0800

    解决 跨域

--- src/views/MessageDetial.vue
+++ src/views/MessageDetial.vue
@@ -2,35 +2,35 @@
    <div>
     <p>{{$route.params.id}}</p>
     <p>id:::{{msg.id}}</p>
-    <p>title:::{{msg.title}}</p>
-    <p>content:::{{msg.content}}</p>
+    <p>title:::{{msg.name}}</p>
    </div>
 </template>
 
 <script>
-    const msgArr =[
-      {id: 1 ,title:'msg01',content:'11...'},
-      {id:2,title:'msg02',content:'22...'},
-      {id:3,title:'msg03',content:'33...'} ,
-      {id:4,title:'msg04',content:'44...'}
-
-    ];
+    var msgArr = [];
     export default {
+
       data(){
         return {
           msg:{}
         }
       },
       mounted(){
-        setTimeout(() => {
           const id = this.$route.params.id;
-          this.msg = msgArr.find(msg => msg.id===id*1 );
-        },1000)
+          //console.log(id);
+          //console.log(this.message);
+
+          //this.msg = this.message.find(msg => msg.id===id*1 );
+        //订阅消息
+        PubSub.subscribe("sendMessage",(msg,message) =>{
+          msgArr = message;
+          this.msg = message.find(msg => msg.id===id*1 );
+        })
       },
       watch:{
         $route:function (value) {
           const id = value.params.id;
-          this.msg = msgArr.find(msg => msg.id===id*1 );
+           this.msg = msgArr.find(msg => msg.id===id*1 );
         }
       }
     }
--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -3,7 +3,8 @@
     <ul>
       <li v-for="(msg,index) in message " :key="msg.id">
       <!--  <a href="`/home/messages/detail/${msg.id}`"></a>-->
-        <router-link :to="`/home/messages/detail/${msg.id}`">{{msg.title}}</router-link>
+        <!--<router-link :to="`/home/messages/detail/${msg.id}`">{{msg.name}}</router-link>-->
+        <router-link :to="`/home/messages/detail/${msg.id}`">{{msg.name}}</router-link>
       </li>
     </ul>
     <hr>
@@ -12,6 +13,7 @@
 </template>
 
 <script>
+  import PubSub from "pubsub-js"
     export default {
       data(){
         return{
@@ -19,16 +21,20 @@
         }
       },
       mounted(){
-        setTimeout(() => {
-          const msgArr =[
-            {id: 1 ,title:'msg01',content:'11...'},
-            {id:2,title:'msg02',content:'22...'},
-            {id:3,title:'msg03',content:'33...'} ,
-            {id:4,title:'msg04',content:'44...'}
+        //var message =[];
+       this.axios.get('/todos/getTodoList')
+          .then(response =>{
+            //console.log(response.data);
+           var message = response.data;
+            this.message = message;
+            PubSub.publish("sendMessage",this.message );
 
-          ];
-          this.message = msgArr;
-        },1000)
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+
       }
     }
 </script>
